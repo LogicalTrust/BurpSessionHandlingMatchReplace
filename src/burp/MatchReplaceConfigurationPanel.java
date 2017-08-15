f7d79cce0a255e2ac9e571817604ff26e26170dc
Michał <michal.dardas@gmail.com>
2017-08-15 11:45:10 +0200

    change url

--- src/burp/MatchReplaceConfigurationPanel.java
+++ src/burp/MatchReplaceConfigurationPanel.java
@@ -99,7 +99,7 @@ public class MatchReplaceConfigurationPanel extends JPanel {
 		add(panel, BorderLayout.NORTH);
 		
 		JLabel patternUrl = createLabelURL("https://docs.oracle.com/javase/8/docs/api/java/util/regex/Pattern.html");
-		JLabel gitHubUrl = createLabelURL("https://github.com/mmmds/BurpSessionHandlingMatchReplace");
+		JLabel gitHubUrl = createLabelURL("https://github.com/LogicalTrust/BurpSessionHandlingMatchReplace");
 		
 		JButton btn = new JButton("?");
 		btn.addActionListener((ev) -> {
